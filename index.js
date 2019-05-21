15f9abf32d5182352392a3baf050da77db45378e
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-21 14:58:03 +0100

    Added Carousel to the Landing page I added carousel to the landing page and also a paragraph tag, and I also fixed some styling

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
-(function () {
-
-    // Create mobile element
-    var mobile = document.createElement('div');
-    mobile.className = 'nav-mobile';
-    document.querySelector('.nav').appendChild(mobile);
-
-    // hasClass
-    function hasClass(elem, className) {
-        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+class Carousel {
+    constructor(element) {
+      this.element = element;
+  
+     
+      this.rightButton = document.querySelector(".right-button");
+      this.leftButton = document.querySelector(".left-button");
+      this.images = document.querySelectorAll(".carousel img");
+  
+      this.rightButton.addEventListener("click", () => this.rightClick());
+      this.leftButton.addEventListener("click", () => this.leftClick());
+  
+      this.images[0].style.display = "block";
+      this.currentIndex = 0;
+  
+      this.images.forEach((image, index) =>
+        image.setAttribute("data-index", index)
+      );
+  
+      this.length = this.images.length - 1;
     }
-
-    // toggleClass
-    function toggleClass(elem, className) {
-        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
-        if (hasClass(elem, className)) {
-            while (newClass.indexOf(' ' + className + ' ') >= 0) {
-                newClass = newClass.replace(' ' + className + ' ', ' ');
-            }
-            elem.className = newClass.replace(/^\s+|\s+$/g, '');
-        } else {
-            elem.className += ' ' + className;
-        }
+  
+    // Methods
+    rightClick() {
+      if (this.currentIndex < this.length) {
+        this.currentIndex += 1;
+  
+        let newImage = document.querySelector(
+          `.carousel img[data-index="${this.currentIndex}"]`
+        );
+        let previousImage = document.querySelector(
+          `.carousel img[data-index="${this.currentIndex - 1}"]`
+        );
+  
+        newImage.style.display = "block";
+        previousImage.style.display = "none";
+      }
     }
-
-    // Mobile nav function
-    var mobileNav = document.querySelector('.nav-mobile');
-    var toggle = document.querySelector('.nav-list');
-    mobileNav.onclick = function () {
-        toggleClass(this, 'nav-mobile-open');
-        toggleClass(toggle, 'nav-active');
-    };
-})();
+  
+    leftClick() {
+      if (this.currentIndex > 0) {
+        this.currentIndex -= 1;
+  
+        let newImage = document.querySelector(
+          `.carousel img[data-index="${this.currentIndex}"]`
+        );
+        let previousImage = document.querySelector(
+          `.carousel img[data-index="${this.currentIndex + 1}"]`
+        );
+  
+        newImage.style.display = "block";
+        
+        previousImage.style.display = "none";
+      }
+    }
+  }
+  
+  let carousel = document.querySelector(".carousel");
+  carousel = new Carousel(carousel);
+  
